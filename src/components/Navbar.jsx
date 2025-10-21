1a76fd9e7006b57933aa847c7ac3c624cfb971a5
wilsq <viljami.j.viinikainen@gmail.com>
2025-10-21 12:51:04 +0300

    Added hamburger menu + dropdown in navbar + css styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
-import { Camera } from "lucide-react";
+import { Camera, X, Menu } from "lucide-react";
+import { useState } from "react";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <nav className="w-full bg-black text-white">
+    <nav className="w-full bg-black text-white relative">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-2 py-4">
         <h1 className="text-2x1 flex gap-2 font-bold text-white-600">
           <Camera /> Jani Peurala Photography
         </h1>
-        <ul className="flex gap-10 font-mono">
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex gap-10 font-mono">
           <Link to="/" className="hover:text-gray-500 cursor-pointer">
             Home
           </Link>
@@ -17,7 +22,44 @@ function Navbar() {
           </Link>
           <li className="hover:text-gray-500 cursor-pointer">About Me</li>
         </ul>
+
+        {/* Mobiili Menu */}
+        <button className="md:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
       </div>
+
+      {/* Mobiili dropdown */}
+      {menuOpen && (
+        <div
+          className="absolute top-full left-0 w-full bg-black border-t border-white/20 md:hidden z-50 shadow-lg
+               animate-slideDown origin-top"
+        >
+          <ul className="flex flex-col items-center gap-6 py-6">
+            <Link
+              to="/"
+              onClick={() => setMenuOpen(false)}
+              className="hover:text-gray-500 transition"
+            >
+              Home
+            </Link>
+            <Link
+              to="/gallery"
+              onClick={() => setMenuOpen(false)}
+              className="hover:text-gray-500 transition"
+            >
+              Gallery
+            </Link>
+            <Link
+              to="/about"
+              onClick={() => setMenuOpen(false)}
+              className="hover:text-gray-500 transition"
+            >
+              About
+            </Link>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
